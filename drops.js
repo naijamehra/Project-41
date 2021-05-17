4d514dc1c9ec33c43bbb45cdb9b97a3752cf537f
naijamehra <67907450+naijamehra@users.noreply.github.com>
2021-05-17 12:41:50 +0530

    Add files via upload

---
+++ drops.js
@@ -0,0 +1,22 @@
+class Drops{
+    constructor(){
+        var options = {
+            'friction':0.001,
+            restitution:0.1
+        } 
+
+        this.rain = Bodies.circle(x,y,5,options);
+        this.radius=5;
+        World.add(world, this.rain);
+    }
+    update(){
+        if(this.rain.position>height){
+            Matter.Body.setPosition(this.rain,{x:random(0,400), y:random(0,400)})
+        }
+    }
+    showDrops(){
+        fill("blue");
+        ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius)
+    }
+}
+
